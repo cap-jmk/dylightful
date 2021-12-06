b199e872d0049e9e7f5896f40934709a8a06c001
Manuel <60146151+MQSchleich@users.noreply.github.com>
2021-12-06 17:40:37 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="continious-template",
+    version="0.1",
+    description="A package supporting rapid python development with GitHub CI",
+    url="https://github.com/MQSchleich/chembee",
+    author="Peter Parker",
+    author_email="julian.m.kleber@gmail.com",
+    license="MIT",
+    packages=["continious-template"],
+    zip_safe=False,
+)
