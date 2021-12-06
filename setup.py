eea252569cd2d92438344bf2e431fee6fc34cd65
Julian M. Kleber <jkdadww@icloud.com>
2021-12-06 19:43:52 +0100

    packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup
 
 setup(
-    name="continious-template",
+    name="dylightful",
     version="0.1",
     description="A package supporting rapid python development with GitHub CI",
-    url="https://github.com/MQSchleich/chembee",
+    url="https://github.com/MQSchleich/dylightful",
     author="Peter Parker",
     author_email="julian.m.kleber@gmail.com",
     license="MIT",
-    packages=["continious-template"],
+    packages=["app", "dylightful"],
+    install_requires=[
+          'numpy',
+          'pandas',
+          'matplotlib',
+          'tqdm',
+          'hmmlearn'
+      ],
     zip_safe=False,
 )
